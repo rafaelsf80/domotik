617400b6aa6de1090cb31f1f2fd7dfee73480c61
rafaelsf80 <rafaelsf80@gmail.com>
2016-11-13 22:37:45 +0100

    SafetyNet updates

--- app/src/main/java/es/rafaelsf80/domotik/app/Attest.java
+++ app/src/main/java/es/rafaelsf80/domotik/app/Attest.java
@@ -1,6 +1,7 @@
 package es.rafaelsf80.domotik.app;
 
 import android.content.Context;
+import android.util.Base64;
 import android.util.Log;
 
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -8,6 +9,10 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.safetynet.SafetyNet;
 import com.google.android.gms.safetynet.SafetyNetApi;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.security.SecureRandom;
 
@@ -52,7 +57,7 @@ public class Attest {
         SecureRandom secureRandom = new SecureRandom();
         secureRandom.nextBytes(nonce);
 
-        Log.d(TAG, "Launching Attestation (SafetyNet)");
+        Log.d(TAG, "Running Attestation (SafetyNet)");
         SafetyNet.SafetyNetApi.attest(mGoogleApiClient, nonce)
                 .setResultCallback(new ResultCallback<SafetyNetApi.AttestationResult>() {
                     @Override
@@ -61,14 +66,67 @@ public class Attest {
                         if (status.isSuccess()) {
                             // Indicates communication with the service was successful.
                             // result.getJwsResult() contains the result data
-                            Log.d(TAG, "Attest successful: " + result.getJwsResult().toString());
+                            Log.d(TAG, "Attest successful: " + result.getJwsResult());
+                            //the JWT (JSON WEB TOKEN) is just a 3 base64 encoded parts concatenated by a . character
+                            final String[] jwtParts = result.getJwsResult().split("\\.");
+
+                            if (jwtParts.length == 3) {
+                                //we're only really interested in the body/payload
+                                String decodedPayload = new String(Base64.decode(jwtParts[1], Base64.DEFAULT));
+
+                                Log.d(TAG, "Attest decode payload: " + parseSafetyNetResponse(decodedPayload));
+                            }
                         } else {
                             // An error occurred while communicating with the service
-                            Log.d(TAG, "Attest ERROR: " + result.getJwsResult().toString());
+                            Log.d(TAG, "Attest ERROR: " + result.getJwsResult());
 
                         }
                     }
                 });
+    }
+
+    public String parseSafetyNetResponse(String decodedJWTPayload) {
+
+        String response = null;
+
+        try {
+            JSONObject root = new JSONObject(decodedJWTPayload);
+            if(root.has("nonce")) {
+                response = "nonce: " + root.getString("nonce") + "\n";
+            }
+
+            if(root.has("timestampMs")) {
+                response += "timeStampMs: " + root.getLong("timestampMs") + "\n";;
+            }
+
+            if(root.has("apkPackageName")) {
+                response += "apkPackageName: " + root.getString("apkPackageName") + "\n";;
+            }
+
+            if(root.has("apkCertificateDigestSha256")) {
+                JSONArray jsonArray = root.getJSONArray("apkCertificateDigestSha256");
+                if(jsonArray!=null){
+                    String[] certDigests = new String[jsonArray.length()];
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        certDigests[i]=jsonArray.getString(i);
+                    }
+                    response += "certDigest: " + certDigests + "\n";
+                }
+            }
+
+            if(root.has("apkDigestSha256")) {
+                response += "apkDigestSha256: " + root.getString("apkDigestSha256") + "\n";;
+            }
+
+            if(root.has("ctsProfileMatch")) {
+                response += "ctsProfileMatch: " + root.getBoolean("ctsProfileMatch") + "\n";;
+            }
+            return response;
+
+        } catch (JSONException e) {
+            Log.e(TAG, "problem parsing decodedJWTPayload:"+ e.getMessage(), e);
+        }
+        return null;
     }
 }
 
